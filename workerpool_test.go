639e1615a082fe3aad61ae5c3bd951256c809e91
Chris James <qui666@gmail.com>
2017-01-20 14:31:39 +0000

    insane, but seems to work?

--- workerpool_test.go
+++ workerpool_test.go
@@ -6,28 +6,14 @@ import (
 	"time"
 )
 
-type testHandler struct {
-	sync        sync.RWMutex
-	invocations int
-}
-
-func (*testHandler) Name() string {
-	return "test worker"
-}
-
-func (w *testHandler) Handle(msg Message) {
-	w.sync.Lock()
-	defer w.sync.Unlock()
-	w.invocations++
-}
-
-//todo: We need to somehow test the max number of workers functionality
 func TestWorkerPool(t *testing.T) {
-	handler := &testHandler{}
+	poolCount := &workerPoolCount{}
+	handler := &testHandler{workerPoolCount:poolCount}
 	messages := make(chan Message, 10)
 	numberOfJobs := 10
+	maxWorkers := 2
 
-	startWorkers(messages, handler, 2, &testLogger{t})
+	startWorkers(messages, handler, maxWorkers, &testLogger{t})
 
 	for i := 0; i < numberOfJobs; i++ {
 		messages <- NewStubMessage("foo", 5*time.Second)
@@ -38,4 +24,51 @@ func TestWorkerPool(t *testing.T) {
 	if handler.invocations != numberOfJobs {
 		t.Error("Handler was not called enough times, expect 10 but got", handler.invocations)
 	}
+
+	if poolCount.max != maxWorkers{
+		t.Error("Expected max workers of", maxWorkers, "but got", poolCount.max)
+	}
+}
+
+type workerPoolCount struct {
+	sync.Mutex
+	currentRunning, max int
+}
+
+func (w *workerPoolCount) incr() {
+	w.Lock()
+	defer w.Unlock()
+
+	w.currentRunning++
+	if w.currentRunning>=w.max{
+		w.max = w.currentRunning
+	}
+}
+
+func (w *workerPoolCount) decr() {
+	w.Lock()
+	defer w.Unlock()
+
+	w.currentRunning--
+}
+
+type testHandler struct {
+	workerPoolCount *workerPoolCount
+	sync.Mutex
+	invocations int
+}
+
+func (*testHandler) Name() string {
+	return "test worker"
+}
+
+func (w *testHandler) Handle(msg Message) {
+	w.workerPoolCount.incr()
+	time.Sleep(10 * time.Millisecond)
+	defer w.workerPoolCount.decr()
+
+	w.Lock()
+	defer w.Unlock()
+
+	w.invocations++
 }
