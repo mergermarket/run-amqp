9231837e0873d29b2357b17fa27d020ccfbe3acb
Chris James <qui666@gmail.com>
2017-01-20 14:17:04 +0000

    basic test for the workerpool

--- workerpool_test.go
+++ workerpool_test.go
@@ -1,13 +1,43 @@
 package runamqp
 
-import "testing"
+import (
+	"testing"
+	"time"
+	"sync"
+)
 
-//todo: testme
+
+type testHandler struct {
+	sync sync.RWMutex
+	invocations int
+}
+
+func (*testHandler) Name() string {
+	return "test worker"
+}
+
+func (w *testHandler) Handle(msg Message) {
+	w.sync.Lock()
+	defer w.sync.Unlock()
+	w.invocations++
+}
+
+//todo: We need to somehow test the max number of workers functionality
 func TestWorkerPool(t *testing.T) {
-	/*
-	Given some test handler and a max number of workers..
+	handler := &testHandler{}
+	messages := make(chan Message, 10)
+	numberOfJobs := 10
 
-	- Ensure handler gets called with message
-	- Fire off lots of messages and somehow ensure that only max number of workers are workin' at a time
-	 */
+	startWorkers(messages, handler, 2, &testLogger{t})
+
+
+	for i:=0; i<numberOfJobs; i++ {
+		messages <- NewStubMessage("foo", 5*time.Second)
+	}
+
+	time.Sleep(1 * time.Second) // this blows
+
+	if handler.invocations != numberOfJobs{
+		t.Error("Handler was not called enough times, expect 10 but got", handler.invocations)
+	}
 }
