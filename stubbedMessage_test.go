6b5b6cccbcd138619dc48c1b7ef441a16c206498
Baktash <Baktash.Ahmed@mergermarket.com>
2017-01-24 19:15:50 +0000

    refactored tests

--- stubbedMessage_test.go
+++ stubbedMessage_test.go
@@ -10,289 +10,295 @@ func TestStubMessage_Body(t *testing.T) {
 	}
 }
 
-func Test_It_Should_Ack_Message_Not_Acked_Previously(t *testing.T) {
+func TestStubMessage_Ack(t *testing.T) {
+	t.Run("Should ack message not acked previously", func(t *testing.T) {
+		msg := NewStubMessage("msg")
 
-	msg := NewStubMessage("msg")
+		if msg.AckCalled() {
+			t.Error("Ack should NOT have been called at this point")
+		}
 
-	if msg.AckCalled() {
-		t.Error("Ack should NOT have been called at this point")
-	}
+		err := msg.Ack()
 
-	err := msg.Ack()
+		if err != nil {
+			t.Error("Should have been able to successfully Ack the message", err)
+		}
 
-	if err != nil {
-		t.Error("Should have been able to successfully Ack the message", err)
-	}
+		if !msg.AckCalled() {
+			t.Error("Message should have been Acked but it was not")
+		}
+	})
 
-	if !msg.AckCalled() {
-		t.Error("Message should have been Acked but it was not")
-	}
+	t.Run("Should NOT ack message previously acked", func(t *testing.T) {
+		msg := NewStubMessage("msg")
+
+		if msg.AckCalled() {
+			t.Error("Ack should NOT have been called at this point")
+		}
+
+		err := msg.Ack()
+
+		if err != nil {
+			t.Error("Should have been able to successfully Ack the message", err)
+		}
+
+		if !msg.AckCalled() {
+			t.Error("Expect ack count to be 1")
+		}
+
+		err = msg.Ack()
+
+		if err == nil {
+			t.Error("Should NOT have been able to Ack a message that has been Acked previously")
+		}
+	})
+
+	t.Run("Should NOT ack message previously nacked", func(t *testing.T) {
+		msg := NewStubMessage("msg")
+
+		if msg.AckCalled() {
+			t.Error("Ack should NOT have been called at this point")
+		}
+
+		err := msg.Nack("Nacking reason")
+
+		if err != nil {
+			t.Error("Should have been able to successfully Nack the message", err)
+		}
+
+		if msg.AckCalled() {
+			t.Error("Ack should NOT have been called at this point")
+		}
+
+		err = msg.Ack()
+
+		if err == nil {
+			t.Error("Should NOT have been able to Ack a message that has been Nacked previously")
+		}
+	})
+
+	t.Run("Should NOT ack message previously requeued", func(t *testing.T) {
+		msg := NewStubMessage("msg")
+
+		if msg.AckCalled() {
+			t.Error("Ack should NOT have been called at this point")
+		}
+
+		err := msg.Requeue("Requeuing reason")
+
+		if err != nil {
+			t.Error("Should have been able to successfully Nack the message", err)
+		}
+
+		if msg.AckCalled() {
+			t.Error("Ack should NOT have been called at this point")
+		}
+
+		err = msg.Ack()
+
+		if err == nil {
+			t.Error("Should NOT have been able to Ack a message that has been Reqeued previously")
+		}
+	})
 }
 
-func Test_It_Should_NOT_Ack_Message_Previously_Acked(t *testing.T) {
-	msg := NewStubMessage("msg")
+func TestStubMessage_Nack(t *testing.T) {
+	t.Run("Should nack message NOT nacked previosly", func(t *testing.T) {
+		msg := NewStubMessage("msg")
 
-	if msg.AckCalled() {
-		t.Error("Ack should NOT have been called at this point")
-	}
+		if msg.NackCalled() {
+			t.Error("Nack should NOT have been called at this point")
+		}
 
-	err := msg.Ack()
+		err := msg.Nack("successful nack")
 
-	if err != nil {
-		t.Error("Should have been able to successfully Ack the message", err)
-	}
+		if err != nil {
+			t.Error("Should have been able to successfully Nack the message", err)
+		}
 
-	if !msg.AckCalled() {
-		t.Error("Expect ack count to be 1")
-	}
+		if !msg.NackCalled() {
+			t.Error("Nack should have been called at this point")
+		}
 
-	err = msg.Ack()
+		if !msg.NackedWith("successful nack") {
+			t.Error("Expected nack reason to be recorded")
+		}
+	})
 
-	if err == nil {
-		t.Error("Should NOT have been able to Ack a message that has been Acked previously")
-	}
+	t.Run("Should NOT nack message previously nacked", func(t *testing.T) {
+		msg := NewStubMessage("msg")
+
+		if msg.NackCalled() {
+			t.Error("Nack should NOT have been called at this point")
+		}
+
+		err := msg.Nack("successful nack")
+
+		if err != nil {
+			t.Error("Should have been able to successfully Nack the message", err)
+		}
+
+		if !msg.NackCalled() {
+			t.Error("Nack should have been called at this point")
+		}
+
+		if !msg.NackedWith("successful nack") {
+			t.Error("Expected nack reason to be recorded")
+		}
+
+		err = msg.Nack("unsuccessful nack")
+
+		if err == nil {
+			t.Error("Should NOT have been able to call the Nack when it was Nacked previously", err)
+		}
+	})
+
+	t.Run("Should NOT nack message previously acked", func(t *testing.T) {
+		msg := NewStubMessage("msg")
+
+		if msg.NackCalled() {
+			t.Error("Nack should NOT have been called at this point")
+		}
+
+		err := msg.Ack()
+
+		if err != nil {
+			t.Error("Should have been able to successfully Ack the message", err)
+		}
+
+		if msg.NackCalled() {
+			t.Error("Nack should NOT have been called at this point")
+		}
+
+		err = msg.Nack("unsuccessful nack")
+
+		if err == nil {
+			t.Error("Should NOT have been able to call the Nack when it was Acked previously")
+		}
+	})
+
+	t.Run("Should NOT nack message previously requeued", func(t *testing.T) {
+		msg := NewStubMessage("msg")
+
+		if msg.NackCalled() {
+			t.Error("Nack should NOT have been called at this point")
+		}
+
+		err := msg.Requeue("successful nack")
+
+		if err != nil {
+			t.Error("Should have been able to successfully Requeue the message", err)
+		}
+
+		if msg.NackCalled() {
+			t.Error("Nack should NOT have been called at this point")
+		}
+
+		err = msg.Nack("unsuccessful nack")
+
+		if err == nil {
+			t.Error("Should NOT have been able to call the Nack when it was Nacked previously")
+		}
+	})
+
 }
 
-func Test_It_Should_NOT_Ack_Message_Previously_Nacked(t *testing.T) {
-	msg := NewStubMessage("msg")
+func TestStubMessage_Requeue(t *testing.T) {
+	t.Run("Should requeue message Not requeued previously", func(t *testing.T) {
+		msg := NewStubMessage("msg")
 
-	if msg.AckCalled() {
-		t.Error("Ack should NOT have been called at this point")
-	}
+		if msg.RequeueCalled() {
+			t.Error("Requeue should NOT have been called at this point")
+		}
 
-	err := msg.Nack("Nacking reason")
+		err := msg.Requeue("successful requeue")
 
-	if err != nil {
-		t.Error("Should have been able to successfully Nack the message", err)
-	}
+		if err != nil {
+			t.Error("Should have been able to successfully Requeue the message", err)
+		}
 
-	if msg.AckCalled() {
-		t.Error("Ack should NOT have been called at this point")
-	}
+		if !msg.RequeueCalled() {
+			t.Error("Requeue should have been called at this point")
+		}
 
-	err = msg.Ack()
+		if !msg.RequeuedWith("successful requeue") {
+			t.Error("Expected requeue to been called with poo but it was not called.")
+		}
+	})
 
-	if err == nil {
-		t.Error("Should NOT have been able to Ack a message that has been Nacked previously")
-	}
-}
+	t.Run("Should NOT requeue message previously requeued", func(t *testing.T) {
+		msg := NewStubMessage("msg")
 
-func Test_It_Should_NOT_Ack_Message_Previously_Requeued(t *testing.T) {
-	msg := NewStubMessage("msg")
+		if msg.RequeueCalled() {
+			t.Error("Requeue should NOT have been called at this point")
+		}
 
-	if msg.AckCalled() {
-		t.Error("Ack should NOT have been called at this point")
-	}
+		err := msg.Requeue("successful requeue")
 
-	err := msg.Requeue("Requeuing reason")
+		if err != nil {
+			t.Error("Should have been able to successfully Requeue the message", err)
+		}
 
-	if err != nil {
-		t.Error("Should have been able to successfully Nack the message", err)
-	}
+		if !msg.RequeueCalled() {
+			t.Error("Requeue should have been called at this point")
+		}
 
-	if msg.AckCalled() {
-		t.Error("Ack should NOT have been called at this point")
-	}
+		if !msg.RequeuedWith("successful requeue") {
+			t.Error("Expected requeue to been called with poo but it was not called.")
+		}
 
-	err = msg.Ack()
+		err = msg.Requeue("unsuccessful requeue")
 
-	if err == nil {
-		t.Error("Should NOT have been able to Ack a message that has been Reqeued previously")
-	}
-}
+		if err == nil {
+			t.Error("Should NOT have been able to Requeue the message when it was Requeued previously")
+		}
+	})
 
-func Test_It_Should_Nack_Message_Not_Nacked_Previosly(t *testing.T) {
-	msg := NewStubMessage("msg")
+	t.Run("Should NOT requeue message previously acked", func(t *testing.T) {
+		msg := NewStubMessage("msg")
 
-	if msg.NackCalled() {
-		t.Error("Nack should NOT have been called at this point")
-	}
+		if msg.RequeueCalled() {
+			t.Error("Requeue should NOT have been called at this point")
+		}
 
-	err := msg.Nack("successful nack")
+		err := msg.Ack()
 
-	if err != nil {
-		t.Error("Should have been able to successfully Nack the message", err)
-	}
+		if err != nil {
+			t.Error("Should have been able to successfully Ack the message", err)
+		}
 
-	if !msg.NackCalled() {
-		t.Error("Nack should have been called at this point")
-	}
+		if msg.RequeueCalled() {
+			t.Error("Requeue should NOT have been called at this point")
+		}
 
-	if !msg.NackedWith("successful nack") {
-		t.Error("Expected nack reason to be recorded")
-	}
-}
+		err = msg.Requeue("unsuccessful requeue")
 
-func Test_It_Should_NOT_Nack_Message_Previously_Nacked(t *testing.T) {
-	msg := NewStubMessage("msg")
+		if err == nil {
+			t.Error("Should NOT have been able to Requeue the message when it was Acked previously")
+		}
+	})
 
-	if msg.NackCalled() {
-		t.Error("Nack should NOT have been called at this point")
-	}
+	t.Run("Should NOT requeue message previously nacked", func(t *testing.T) {
+		msg := NewStubMessage("msg")
 
-	err := msg.Nack("successful nack")
+		if msg.RequeueCalled() {
+			t.Error("Requeue should NOT have been called at this point")
+		}
 
-	if err != nil {
-		t.Error("Should have been able to successfully Nack the message", err)
-	}
+		err := msg.Nack("successful nack")
 
-	if !msg.NackCalled() {
-		t.Error("Nack should have been called at this point")
-	}
+		if err != nil {
+			t.Error("Should have been able to successfully Nack the message", err)
+		}
 
-	if !msg.NackedWith("successful nack") {
-		t.Error("Expected nack reason to be recorded")
-	}
+		if msg.RequeueCalled() {
+			t.Error("Requeue should NOT have been called at this point")
+		}
 
-	err = msg.Nack("unsuccessful nack")
+		err = msg.Requeue("unsuccessful requeue")
 
-	if err == nil {
-		t.Error("Should NOT have been able to call the Nack when it was Nacked previously", err)
-	}
-}
-
-func Test_It_Should_NOT_Nack_Message_Previously_Acked(t *testing.T) {
-	msg := NewStubMessage("msg")
-
-	if msg.NackCalled() {
-		t.Error("Nack should NOT have been called at this point")
-	}
-
-	err := msg.Ack()
-
-	if err != nil {
-		t.Error("Should have been able to successfully Ack the message", err)
-	}
-
-	if msg.NackCalled() {
-		t.Error("Nack should NOT have been called at this point")
-	}
-
-	err = msg.Nack("unsuccessful nack")
-
-	if err == nil {
-		t.Error("Should NOT have been able to call the Nack when it was Acked previously")
-	}
-}
-
-func Test_It_Should_NOT_Nack_Message_Previously_Requeued(t *testing.T) {
-	msg := NewStubMessage("msg")
-
-	if msg.NackCalled() {
-		t.Error("Nack should NOT have been called at this point")
-	}
-
-	err := msg.Requeue("successful nack")
-
-	if err != nil {
-		t.Error("Should have been able to successfully Requeue the message", err)
-	}
-
-	if msg.NackCalled() {
-		t.Error("Nack should NOT have been called at this point")
-	}
-
-	err = msg.Nack("unsuccessful nack")
-
-	if err == nil {
-		t.Error("Should NOT have been able to call the Nack when it was Nacked previously")
-	}
-}
-
-func Test_It_Should_Requeue_Message_Not_Requeued_Previously(t *testing.T) {
-	msg := NewStubMessage("msg")
-
-	if msg.RequeueCalled() {
-		t.Error("Requeue should NOT have been called at this point")
-	}
-
-	err := msg.Requeue("successful requeue")
-
-	if err != nil {
-		t.Error("Should have been able to successfully Requeue the message", err)
-	}
-
-	if !msg.RequeueCalled() {
-		t.Error("Requeue should have been called at this point")
-	}
-
-	if !msg.RequeuedWith("successful requeue") {
-		t.Error("Expected requeue to been called with poo but it was not called.")
-	}
-}
-
-func Test_It_Should_NOT_Requeue_Message_Previously_Requeued(t *testing.T) {
-	msg := NewStubMessage("msg")
-
-	if msg.RequeueCalled() {
-		t.Error("Requeue should NOT have been called at this point")
-	}
-
-	err := msg.Requeue("successful requeue")
-
-	if err != nil {
-		t.Error("Should have been able to successfully Requeue the message", err)
-	}
-
-	if !msg.RequeueCalled() {
-		t.Error("Requeue should have been called at this point")
-	}
-
-	if !msg.RequeuedWith("successful requeue") {
-		t.Error("Expected requeue to been called with poo but it was not called.")
-	}
-
-	err = msg.Requeue("unsuccessful requeue")
-
-	if err == nil {
-		t.Error("Should NOT have been able to Requeue the message when it was Requeued previously")
-	}
-}
-
-func Test_It_Should_NOT_Requeue_Message_Previously_Acked(t *testing.T) {
-	msg := NewStubMessage("msg")
-
-	if msg.RequeueCalled() {
-		t.Error("Requeue should NOT have been called at this point")
-	}
-
-	err := msg.Ack()
-
-	if err != nil {
-		t.Error("Should have been able to successfully Ack the message", err)
-	}
-
-	if msg.RequeueCalled() {
-		t.Error("Requeue should NOT have been called at this point")
-	}
-
-	err = msg.Requeue("unsuccessful requeue")
-
-	if err == nil {
-		t.Error("Should NOT have been able to Requeue the message when it was Acked previously")
-	}
-}
-
-func Test_It_Should_NOT_Requeue_Message_Previously_Nacked(t *testing.T) {
-	msg := NewStubMessage("msg")
-
-	if msg.RequeueCalled() {
-		t.Error("Requeue should NOT have been called at this point")
-	}
-
-	err := msg.Nack("successful nack")
-
-	if err != nil {
-		t.Error("Should have been able to successfully Nack the message", err)
-	}
-
-	if msg.RequeueCalled() {
-		t.Error("Requeue should NOT have been called at this point")
-	}
-
-	err = msg.Requeue("unsuccessful requeue")
-
-	if err == nil {
-		t.Error("Should NOT have been able to Requeue the message when it was Nacked previously")
-	}
+		if err == nil {
+			t.Error("Should NOT have been able to Requeue the message when it was Nacked previously")
+		}
+	})
 }
